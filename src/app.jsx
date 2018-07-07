52de098aa4364f40db7b3f457482ebed40d0dd19
agierens <apgierens@gmail.com>
2018-07-07 22:07:37 +1000

    - Added: Basic stuff. Commiting so dad can see in prod

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainPage from './pages/main-page';
+import MainPage from './pages/main-page/main-page';
 
 // Render app
-ReactDOM.render(<MainPage />, document.querySelector('#root'));
+ReactDOM.render(<MainPage />, document.querySelector('#root')); //eslint-disable-line
